df54f9afeac0834539f341b9c3b1a8f79af9af70
unknown <anahgirao@gmail.com>
2019-08-28 12:40:31 -0300

    algumas views e templates

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from users.models import User
+from django.views import generic
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy 
 # Create your views here.
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'    
+
+class UserLogoutView(LoginRequiredMixin, LogoutView):
+    pass
+
