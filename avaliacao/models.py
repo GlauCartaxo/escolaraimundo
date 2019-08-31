64110c9766af358c5f43bfd4bab523da5018baf5
unknown <anahgirao@gmail.com>
2019-08-31 17:56:01 -0300

    app avaliacao criado, fazer  modificações na models

---
+++ avaliacao/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from  django.contrib.auth.models import User
+# Create your models here.
+
+class Avaliacao(models.Model):
+    professor = models.ForeignKey("users.User", verbose_name=("professor"),
+    related_name='AvaliacaoProAluno',  on_delete=models.CASCADE)
+    aluno = models.ForeignKey("users.User", verbose_name=("aluno"), on_delete=models.CASCADE)
+    nota = models.CharField()
+
+    #def str(self):
+    #    return f'Nota {self.pk} | Author {self.author} | Created at {self.created_at}'
